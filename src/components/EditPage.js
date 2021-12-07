74e203a3a62df84b982340587a5150b29087b9a6
Govardhan8 <govardhanmurthy007@gmail.com>
2021-12-07 12:32:40 +0530

    added edit on 12hours timestamp

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -25,9 +25,9 @@ export const categories = [
 function refreshPage() {
 	window.location.reload(false)
 }
-const AddForm = ({ click }) => {
+const EditForm = ({ data, edit, click }) => {
 	const submitForm = (values) => {
-		axios.post(`${api_url}/transactions/add`, values).then(() => {
+		axios.put(`${api_url}/transactions/${data._id}`, values).then(() => {
 			click()
 			refreshPage()
 		})
@@ -47,22 +47,25 @@ const AddForm = ({ click }) => {
 	const { values, handleSubmit, handleChange, handleBlur, errors, touched } =
 		useFormik({
 			initialValues: {
-				description: '',
-				date: '',
-				amount: '',
-				division: '',
-				category: '',
-				type: '',
+				description: data.description,
+				date: data.date,
+				amount: data.amount,
+				division: data.division,
+				category: data.category,
+				type: data.type,
 			},
 			validationSchema: validations,
 			onSubmit: (values) => {
-				submitForm(values)
+				if (!edit) {
+					submitForm(values)
+				}
 			},
 		})
 
 	return (
 		<form className='form' onSubmit={handleSubmit}>
 			<div className='relative'>
+				<span>Edit page</span>
 				<CloseSharpIcon
 					className='close'
 					onClick={() => {
@@ -148,11 +151,11 @@ const AddForm = ({ click }) => {
 			<span style={{ color: 'red', fontSize: '0.75rem', marginLeft: '1rem' }}>
 				{errors.type && touched.type && errors.type}
 			</span>
-			<Button variant='contained' type='submit'>
+			<Button variant='contained' type='submit' disabled={!edit}>
 				SUBMIT
 			</Button>
 		</form>
 	)
 }
 
-export default AddForm
+export default EditForm
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,24 +1,51 @@
+import { useState } from 'react'
+import EditForm from './EditPage'
 import './transaction.css'
 
 const Transactions = ({ data }) => {
+	const [editPage, setEditPage] = useState(false)
 	const date = new Date(data.date)
-	const newDate =
-		date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
+	const month = date.getMonth() + 1
+	const newDate = date.getDate() + '/' + month + '/' + date.getFullYear()
+	const editOption = () => {
+		const timestamp = new Date(data.timestamp)
+		const currDate = new Date()
+		const edit = timestamp.getTime() > currDate.getTime() - 12 * 60 * 60 * 100
+		return edit
+	}
+	const edit = editOption()
 	return (
-		<div className='transactions'>
-			<p className='category'>{data.category}</p>
-			<div>
-				<p
-					className='amount'
-					style={{
-						color: data.type.toLowerCase() === 'income' ? '#12e42b' : 'red',
-					}}
-				>
-					₹{data.amount}
-				</p>
-				<p className='date'>{newDate}</p>
+		<>
+			<div
+				className='transactions'
+				onClick={() => {
+					setEditPage(!editPage)
+					console.log(data.timestamp, edit)
+				}}
+			>
+				<p className='category'>{data.category}</p>
+				<div>
+					<p
+						className='amount'
+						style={{
+							color: data.type.toLowerCase() === 'income' ? '#12e42b' : 'red',
+						}}
+					>
+						₹{data.amount}
+					</p>
+					<p className='date'>{newDate}</p>
+				</div>
 			</div>
-		</div>
+			{editPage && (
+				<EditForm
+					data={data}
+					click={() => {
+						setEditPage(false)
+					}}
+					edit={edit}
+				/>
+			)}
+		</>
 	)
 }
 
